aef98d3a3d99a08328cfd1942297e57085612815
Gabriel Bartholdy <gabrielbartholdy07@gmail.com>
2025-02-26 20:27:52 -0300

    att

--- crud.py
+++ crud.py
@@ -2,7 +2,7 @@ import mysql.connector
 from config import MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE 
 
 def get_connection():
-    return mysql.connector.conect(
+    return mysql.connector.connect(
         host = MYSQL_HOST,
         user = MYSQL_USER,
         password = MYSQL_PASSWORD,
