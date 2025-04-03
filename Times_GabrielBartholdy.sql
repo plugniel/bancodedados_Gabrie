e4c0f72daf1cbacfde91d5b27fd457e971445ee2
Gabriel Bartholdy <gabrielbartholdy07@gmail.com>
2025-04-02 21:46:59 -0300

    aa

---
+++ Times_GabrielBartholdy.sql
@@ -0,0 +1,124 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: time_gabrielbart
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contrato`
+--
+
+DROP TABLE IF EXISTS `contrato`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contrato` (
+  `cdTime` int NOT NULL,
+  `cdJogador` int NOT NULL,
+  `dtcontrato` date NOT NULL,
+  `dtrecisao` date DEFAULT NULL,
+  PRIMARY KEY (`cdTime`,`cdJogador`,`dtcontrato`),
+  KEY `fk_contrato_cdjogador` (`cdJogador`),
+  CONSTRAINT `fk_contrato_cdjogador` FOREIGN KEY (`cdJogador`) REFERENCES `jogadores` (`cdjogador`),
+  CONSTRAINT `fk_contrato_cdTime` FOREIGN KEY (`cdTime`) REFERENCES `times` (`cdtime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contrato`
+--
+
+LOCK TABLES `contrato` WRITE;
+/*!40000 ALTER TABLE `contrato` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contrato` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jogadores`
+--
+
+DROP TABLE IF EXISTS `jogadores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `jogadores` (
+  `nomejogador` varchar(35) DEFAULT NULL,
+  `cdjogador` int NOT NULL,
+  PRIMARY KEY (`cdjogador`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jogadores`
+--
+
+LOCK TABLES `jogadores` WRITE;
+/*!40000 ALTER TABLE `jogadores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `jogadores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tiemes`
+--
+
+DROP TABLE IF EXISTS `tiemes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tiemes` (
+  `NomeTime` varchar(30) DEFAULT NULL,
+  `cdTime` int NOT NULL,
+  PRIMARY KEY (`cdTime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tiemes`
+--
+
+LOCK TABLES `tiemes` WRITE;
+/*!40000 ALTER TABLE `tiemes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tiemes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `times`
+--
+
+DROP TABLE IF EXISTS `times`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `times` (
+  `nometime` varchar(35) DEFAULT NULL,
+  `cdtime` int NOT NULL,
+  PRIMARY KEY (`cdtime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `times`
+--
+
+LOCK TABLES `times` WRITE;
+/*!40000 ALTER TABLE `times` DISABLE KEYS */;
+/*!40000 ALTER TABLE `times` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-01 21:40:29
